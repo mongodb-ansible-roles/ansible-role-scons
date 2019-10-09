ee4495ddc45a7a9f5edf9aeb485ea99391f3446b
John Chen <johnchen456@gmail.com>
2019-10-09 12:07:54 -0400

    test: change test stdout to single line

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -7,11 +7,7 @@ end
 # rubocop:disable LineLength
 
 describe command 'scons --version' do
-  its('stdout') { should eq "SCons by Steven Knight et al.:
-	script: v2.4.1.rel_2.4.1:3453:73fefd3ea0b0, 2015/11/09 03:25:05, by bdbaddog on ubuntu1404-32bit
-	engine: v2.4.1.rel_2.4.1:3453:73fefd3ea0b0, 2015/11/09 03:25:05, by bdbaddog on ubuntu1404-32bit
-	engine path: ['/usr/local/lib/scons-2.4.1/SCons']
-Copyright (c) 2001 - 2015 The SCons Foundation\n" }
+  its('stdout') { should eq "SCons by Steven Knight et al.:\n	script: v2.4.1.rel_2.4.1:3453:73fefd3ea0b0, 2015/11/09 03:25:05, by bdbaddog on ubuntu1404-32bit\n	engine: v2.4.1.rel_2.4.1:3453:73fefd3ea0b0, 2015/11/09 03:25:05, by bdbaddog on ubuntu1404-32bit\n	engine path: ['/usr/local/lib/scons-2.4.1/SCons']\nCopyright (c) 2001 - 2015 The SCons Foundation\n" }
 end
 
 # rubocop:enable LineLength
